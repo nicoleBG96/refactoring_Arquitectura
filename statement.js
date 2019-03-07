a05faad2f83691e003832e299d4bf4a7bf1fa9a3
nicole <nikybg96@gmail.com>
2019-03-06 22:08:12 -0400

    apply methods of statementData in renderPlainText

--- statement.js
+++ statement.js
@@ -13,8 +13,8 @@ function  renderPlainText (data, plays) {
     result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   } 
 
-  result += `Amount owed is ${usd(totalAmount ())}\n`;
-  result += `You earned ${totalVolumeCredits () } credits\n`;
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
 }
 
