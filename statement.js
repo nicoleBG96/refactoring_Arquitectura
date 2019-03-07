f4858243fb07f826cd639760921f3fb59516fd17
nicole <nikybg96@gmail.com>
2019-03-06 21:45:24 -0400

    change playFor for .play

--- statement.js
+++ statement.js
@@ -8,7 +8,7 @@ function statement (invoice, plays) {
 function  renderPlainText (data, plays) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
-    result += ` ${playFor (perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   } 
 
   result += `Amount owed is ${usd(totalAmount ())}\n`;
@@ -24,7 +24,7 @@ function enrichPerformance(aPerformance) {
 
   function amountFor (aPerformance) {
     let result = 0;
-    switch (playFor (aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
       result = 40000;
         if (aPerformance.audience > 30) {
@@ -39,7 +39,7 @@ function enrichPerformance(aPerformance) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${playFor (aPerformance).type}`);
+        throw new Error(`unknown type: ${aPerformance.play.type}`);
     }
     return result;
   }
@@ -51,7 +51,7 @@ function enrichPerformance(aPerformance) {
   function volumeCreditsFor (aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor (aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 
