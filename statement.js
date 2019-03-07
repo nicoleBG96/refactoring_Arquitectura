13e2f65d59cc65b1f2a5942f4bfd804059803b64
nicole <nikybg96@gmail.com>
2019-03-06 21:35:29 -0400

    function renderPlainText

--- statement.js
+++ statement.js
@@ -1,14 +1,17 @@
 function statement (invoice, plays) {
+  return renderPlainText (invoice, plays);
+}
+
+function  renderPlainText (invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
-    
-    // print line for this order
     result += ` ${playFor (perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-  }
+  } 
 
   result += `Amount owed is ${usd(totalAmount ())}\n`;
   result += `You earned ${totalVolumeCredits () } credits\n`;
   return result;
+}
 
   function amountFor (aPerformance) {
     let result = 0;
@@ -65,8 +68,4 @@ function statement (invoice, plays) {
     return result;
   }
 
-  function statement (invoice, plays) {
-    return renderPlainText (invoice, plays);
-  }
-
-}
+  
