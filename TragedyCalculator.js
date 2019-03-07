c01b8bef1f35ca7dea479066df744b0e734b37ef
nicole <nikybg96@gmail.com>
2019-03-06 22:47:11 -0400

    refactor tragedyCalculator

--- TragedyCalculator.js
+++ TragedyCalculator.js
@@ -1,3 +1,9 @@
 class TragedyCalculator extends PerformanceCalculator {
-    
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience -30);
+        }
+        return result;
+    }
 }
